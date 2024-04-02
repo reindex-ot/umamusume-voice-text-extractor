aa3a783b5ab3c18be39c26d4fbc7cf25bd1124e8
Re*Index. (ot_inc) <32851879+reindex-ot@users.noreply.github.com>
2024-04-03 00:41:18 +0900

    Add files via upload

---
+++ gui/qtui/main_ui_ja.ts
@@ -0,0 +1,338 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="ja_JP">
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="main_ui.py" line="541"/>
+        <source>Umamusume Voice Text Extractor</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="main_ui.py" line="542"/>
+        <source>Character List</source>
+        <translation type="unfinished">ウマ娘の一覧</translation>
+    </message>
+    <message>
+        <location filename="main_ui.py" line="576"/>
+        <source>Config</source>
+        <translation type="unfinished">設定</translation>
+    </message>
+    <message>
+        <location filename="main_ui.py" line="579"/>
+        <source>./save</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="main_ui.py" line="577"/>
+        <source>select</source>
+        <translation type="unfinished">選択</translation>
+    </message>
+    <message>
+        <location filename="main_ui.py" line="578"/>
+        <source>Save path</source>
+        <translation type="unfinished">保存先</translation>
+    </message>
+    <message>
+        <location filename="main_ui.py" line="580"/>
+        <source>Proxy</source>
+        <translation type="unfinished">プロキシ</translation>
+    </message>
+    <message>
+        <location filename="main_ui.py" line="581"/>
+        <source>eg. http://127.0.0.1:10087</source>
+        <translation type="unfinished">例.:http://127.0.0.1:10087</translation>
+    </message>
+    <message>
+        <location filename="main_ui.py" line="582"/>
+        <source>Use Proxy</source>
+        <translation type="unfinished">プロキシを使用する</translation>
+    </message>
+    <message>
+        <location filename="main_ui.py" line="586"/>
+        <source>Wave Format</source>
+        <translation type="unfinished">Wave形式</translation>
+    </message>
+    <message>
+        <location filename="main_ui.py" line="587"/>
+        <source>Rate: </source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="main_ui.py" line="552"/>
+        <source>22050</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="main_ui.py" line="589"/>
+        <source>Bits: </source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="main_ui.py" line="590"/>
+        <source>16</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="main_ui.py" line="591"/>
+        <source>Channels: </source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="main_ui.py" line="601"/>
+        <source>1</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="main_ui.py" line="557"/>
+        <source>Get voice from all stories</source>
+        <translation type="unfinished">すべてのストーリーから音声を取得</translation>
+    </message>
+    <message>
+        <location filename="main_ui.py" line="558"/>
+        <source>Download missing voice files</source>
+        <translation type="unfinished">不足している音声ファイルをダウンロード</translation>
+    </message>
+    <message>
+        <location filename="main_ui.py" line="559"/>
+        <source>Use cache</source>
+        <translation type="unfinished">キャッシュを使用する</translation>
+    </message>
+    <message>
+        <location filename="main_ui.py" line="560"/>
+        <source>Character ID</source>
+        <translation type="unfinished">ウマ娘のID</translation>
+    </message>
+    <message>
+        <location filename="main_ui.py" line="568"/>
+        <source>Start</source>
+        <translation type="unfinished">開始</translation>
+    </message>
+    <message>
+        <location filename="main_ui.py" line="562"/>
+        <source>Single Character Mode</source>
+        <translation type="unfinished">ウマ娘単一抽出モード</translation>
+    </message>
+    <message>
+        <location filename="main_ui.py" line="564"/>
+        <source>CharaID</source>
+        <translation type="unfinished">ウマ娘のID</translation>
+    </message>
+    <message>
+        <location filename="main_ui.py" line="566"/>
+        <source>SaveID</source>
+        <translation type="unfinished">IDを保存</translation>
+    </message>
+    <message>
+        <location filename="main_ui.py" line="608"/>
+        <source>Remove Selecting</source>
+        <translation type="unfinished">選択中の物を削除</translation>
+    </message>
+    <message>
+        <location filename="main_ui.py" line="569"/>
+        <source>Multi Character Mode</source>
+        <translation type="unfinished">マルチキャラクターモード</translation>
+    </message>
+    <message>
+        <location filename="main_ui.py" line="570"/>
+        <source>Voice Extractor</source>
+        <translation type="unfinished">音声を抽出</translation>
+    </message>
+    <message>
+        <location filename="main_ui.py" line="574"/>
+        <source>Music List</source>
+        <translation type="unfinished">音楽の一覧</translation>
+    </message>
+    <message>
+        <location filename="main_ui.py" line="572"/>
+        <source>Singing Character List</source>
+        <translation type="unfinished">歌唱するウマ娘の一覧</translation>
+    </message>
+    <message>
+        <location filename="main_ui.py" line="583"/>
+        <source>Download missing files</source>
+        <translation type="unfinished">不足しているファイルをダウンロード</translation>
+    </message>
+    <message>
+        <location filename="main_ui.py" line="584"/>
+        <source>Music ID</source>
+        <translation type="unfinished">音楽のID</translation>
+    </message>
+    <message>
+        <location filename="main_ui.py" line="585"/>
+        <source>Chara ID</source>
+        <translation type="unfinished">ウマ娘のID</translation>
+    </message>
+    <message>
+        <location filename="main_ui.py" line="593"/>
+        <source>Extract BGM</source>
+        <translation type="unfinished">BGMを抽出</translation>
+    </message>
+    <message>
+        <location filename="main_ui.py" line="594"/>
+        <source>Extract Full Chara Sound</source>
+        <translation type="unfinished">ウマ娘の音源を完全に抽出</translation>
+    </message>
+    <message>
+        <location filename="main_ui.py" line="595"/>
+        <source>Extract Chara Sound By Lrc</source>
+        <translation type="unfinished">歌詞からウマ娘の音源を抽出</translation>
+    </message>
+    <message>
+        <location filename="main_ui.py" line="596"/>
+        <source>Remove Silence Part</source>
+        <translation type="unfinished">無音の部分を削除する</translation>
+    </message>
+    <message>
+        <location filename="main_ui.py" line="597"/>
+        <source>Extractor</source>
+        <translation type="unfinished">抽出</translation>
+    </message>
+    <message>
+        <location filename="main_ui.py" line="598"/>
+        <source>Position Sets</source>
+        <translation type="unfinished">ポジションの設定</translation>
+    </message>
+    <message>
+        <location filename="main_ui.py" line="599"/>
+        <source>Singing Pos Count:</source>
+        <translation type="unfinished">歌唱担当のポジション数：</translation>
+    </message>
+    <message>
+        <location filename="main_ui.py" line="600"/>
+        <source>0</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="main_ui.py" line="602"/>
+        <source>2</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="main_ui.py" line="603"/>
+        <source>3</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="main_ui.py" line="604"/>
+        <source>4</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="main_ui.py" line="605"/>
+        <source>5</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="main_ui.py" line="606"/>
+        <source>6</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="main_ui.py" line="607"/>
+        <source>7</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="main_ui.py" line="609"/>
+        <source>Clear</source>
+        <translation type="unfinished">クリア</translation>
+    </message>
+    <message>
+        <location filename="main_ui.py" line="610"/>
+        <source>Mix Config</source>
+        <translation type="unfinished">オーディオミックスの設定</translation>
+    </message>
+    <message>
+        <location filename="main_ui.py" line="615"/>
+        <source>Force All Singing</source>
+        <translation type="unfinished">全員に強制的に歌わせる</translation>
+    </message>
+    <message>
+        <location filename="main_ui.py" line="614"/>
+        <source>Start Mix</source>
+        <translation type="unfinished">ミックスを開始</translation>
+    </message>
+    <message>
+        <location filename="main_ui.py" line="616"/>
+        <source>Mixer</source>
+        <translation type="unfinished">ミキサー</translation>
+    </message>
+    <message>
+        <location filename="main_ui.py" line="547"/>
+        <source>Live Music Extractor</source>
+        <translation type="obsolete">ライブ音源の抽出</translation>
+    </message>
+    <message>
+        <location filename="main_ui.py" line="611"/>
+        <source>Character Volume</source>
+        <translation type="unfinished">ウマ娘の音量</translation>
+    </message>
+    <message>
+        <location filename="main_ui.py" line="573"/>
+        <source>Live Music</source>
+        <translation type="unfinished">ライブの音楽</translation>
+    </message>
+    <message>
+        <location filename="main_ui.py" line="575"/>
+        <source>Jukebox Music</source>
+        <translation type="unfinished">ジュークボックスの音楽</translation>
+    </message>
+    <message>
+        <location filename="main_ui.py" line="588"/>
+        <source>48000</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="main_ui.py" line="617"/>
+        <source>Music Extractor</source>
+        <translation type="unfinished">音楽の抽出</translation>
+    </message>
+    <message>
+        <location filename="main_ui.py" line="619"/>
+        <source>Extracted Volume</source>
+        <translation type="unfinished">抽出時の音量</translation>
+    </message>
+    <message>
+        <location filename="main_ui.py" line="620"/>
+        <source>1.0</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="main_ui.py" line="621"/>
+        <source>About</source>
+        <translation type="unfinished">バージョン情報</translation>
+    </message>
+    <message>
+        <location filename="main_ui.py" line="622"/>
+        <source>&lt;!DOCTYPE HTML PUBLIC &quot;-//W3C//DTD HTML 4.0//EN&quot; &quot;http://www.w3.org/TR/REC-html40/strict.dtd&quot;&gt;
+&lt;html&gt;&lt;head&gt;&lt;meta name=&quot;qrichtext&quot; content=&quot;1&quot; /&gt;&lt;style type=&quot;text/css&quot;&gt;
+p, li { white-space: pre-wrap; }
+&lt;/style&gt;&lt;/head&gt;&lt;body style=&quot; font-family:&apos;Microsoft YaHei&apos;; font-size:9pt; font-weight:400; font-style:normal;&quot;&gt;
+&lt;p style=&quot; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;&lt;span style=&quot; font-size:11pt; font-weight:600;&quot;&gt;This Repo&lt;/span&gt;&lt;/p&gt;
+&lt;p style=&quot; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;&lt;a href=&quot;https://github.com/chinosk6/umamusume-voice-text-extractor&quot;&gt;&lt;span style=&quot; font-size:11pt; text-decoration: underline; color:#0000ff;&quot;&gt;chinosk6/umamusume-voice-text-extractor&lt;/span&gt;&lt;/a&gt;&lt;/p&gt;
+&lt;p style=&quot;-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px; font-size:11pt;&quot;&gt;&lt;br /&gt;&lt;/p&gt;
+&lt;p style=&quot; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;&lt;span style=&quot; font-size:11pt; font-weight:600;&quot;&gt;Credits&lt;/span&gt;&lt;/p&gt;
+&lt;p style=&quot; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;&lt;a href=&quot;https://github.com/MarshmallowAndroid/UmaMusumeExplorer&quot;&gt;&lt;span style=&quot; font-size:11pt; text-decoration: underline; color:#0000ff;&quot;&gt;MarshmallowAndroid/UmaMusumeExplorer&lt;/span&gt;&lt;/a&gt;&lt;/p&gt;
+&lt;p style=&quot;-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px; font-size:11pt; color:#0000ff;&quot;&gt;&lt;br /&gt;&lt;/p&gt;
+&lt;p style=&quot; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;&lt;span style=&quot; font-size:11pt; font-weight:600;&quot;&gt;Copyright&lt;/span&gt;&lt;/p&gt;
+&lt;p style=&quot; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;&lt;a href=&quot;https://www.cygames.co.jp&quot;&gt;&lt;span style=&quot; font-size:11pt; text-decoration: underline; color:#0000ff;&quot;&gt;Cygames&lt;/span&gt;&lt;/a&gt; &lt;a href=&quot;https://umamusume.jp&quot;&gt;&lt;span style=&quot; font-size:11pt; text-decoration: underline; color:#0000ff;&quot;&gt;Umamusume&lt;/span&gt;&lt;/a&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="main_ui.py" line="634"/>
+        <source>More Settings</source>
+        <translation type="unfinished">その他の設定</translation>
+    </message>
+    <message>
+        <location filename="main_ui.py" line="618"/>
+        <source>Global Config</source>
+        <translation type="unfinished">一般設定</translation>
+    </message>
+    <message>
+        <location filename="main_ui.py" line="613"/>
+        <source>Leave blank to automatically balance the loudness. -1 will not change volume. 1.0 is the full volume.</source>
+        <translation type="unfinished">ラウドネスのバランスを自動的に調整する場合は空白のままにします。-1では音量は変わりません、フルボリュームは1.0です。</translation>
+    </message>
+</context>
+</TS>
